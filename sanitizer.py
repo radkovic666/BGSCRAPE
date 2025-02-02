9c51e74648c86f94c46bcd7ce16a12703d5cfa61
TodorJivkov <150471788+radkovic666@users.noreply.github.com>
2025-02-02 19:54:22 +0100

    Update sanitizer.py

--- sanitizer.py
+++ sanitizer.py
@@ -6,13 +6,13 @@ import time
 print("")
 print("Applying sanitizer")
 # List of files to delete
-files_to_delete = ["temp.txt", "scrapelog.txt"]
+files_to_delete = ["temp.txt", "scrapelog.txt", "playlist.m3u"]
 
 for file_name in files_to_delete:
     if os.path.exists(file_name):
         try:
             os.remove(file_name)
-            print(f"Deleted {file_name}")
+            print(f"Wiped {file_name}")
         except Exception as e:
             print(f"Error deleting {file_name}: {e}")
     else:
